220cb41c938857df15c1ab16900ac44aca6cf293
Chris <33991025+moimeme81@users.noreply.github.com>
2026-04-09 22:31:59 -0400

    Create const.py

---
+++ custom_components/regie_essence_quebec/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Régie Essence Québec integration."""
+
+DOMAIN = "regie_essence_quebec"
+
+DEFAULT_SCAN_INTERVAL_MINUTES = 60
+
+CONF_LATITUDE = "latitude"
+CONF_LONGITUDE = "longitude"
+CONF_ADDRESS = "address"
+CONF_SCAN_INTERVAL = "scan_interval"
